f2e58a15a56ea05eaf2f32bf335d2630e751aba0
Patrik Mojzis <patrikmojzis@hotmail.com>
2023-02-21 11:44:50 +0100

    del fetch receipt, check if ticket index is set

--- src/ExpoPushNotifications.php
+++ src/ExpoPushNotifications.php
@@ -3,7 +3,6 @@
 namespace Relative\LaravelExpoPushNotifications;
 
 use Illuminate\Notifications\Notification;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Ramsey\Uuid\Uuid;
@@ -11,6 +10,7 @@ use Relative\LaravelExpoPushNotifications\Models\PushToken;
 
 class ExpoPushNotifications
 {
+    const EXPIRE_FOR_STATUSES = ['DeviceNotRegistered', 'InvalidCredentials'];
     private string $baseUrl;
 
     public function __construct()
@@ -25,12 +25,11 @@ class ExpoPushNotifications
         $message->to = $notifiable->pushTokens()->active()->get()->pluck('token')->toArray();
 
         $tickets = $this->dispatch($message);
-        $pushNotifications = collect($message->to)->map(function ($token, $index) use ($message, $tickets, $notifiable, $batchId) {
-            return $this->createPushNotificationRecord($tickets[$index], $token, $batchId, $message, $notifiable);
+        collect($message->to)->each(function ($token, $index) use ($message, $tickets, $notifiable, $batchId) {
+            if (isset($tickets[$index])) {
+                $this->createPushNotificationRecord($tickets[$index], $token, $batchId, $message, $notifiable);
+            }
         });
-
-        $receipts = collect($this->fetchReceipts($pushNotifications->pluck('ticket')->toArray()))->values();
-        $this->updatePushNotificationReceiptStatuses($tickets, $pushNotifications, $receipts);
     }
 
     public function dispatch($pushNotification): array
@@ -40,19 +39,6 @@ class ExpoPushNotifications
             'Accept-encoding' => 'gzip, deflate',
             'Content-Type' => 'application/json'
         ])->post("{$this->baseUrl}/send", (array)$pushNotification);
-        $responseJson = $response->json();
-        return $responseJson['data'] ?? [];
-    }
-
-    public function fetchReceipts(array $tickets): array
-    {
-        $response = Http::withHeaders([
-            'Accept' => 'application/json',
-            'Accept-encoding' => 'gzip, deflate',
-            'Content-Type' => 'application/json'
-        ])->post("$this->baseUrl/getReceipts", [
-            'ids' => $tickets
-        ]);
 
         return $response->json()['data'];
     }
@@ -68,43 +54,23 @@ class ExpoPushNotifications
     public function createPushNotificationRecord($ticket, $token, \Ramsey\Uuid\UuidInterface $batchId, $notification, $notifiable)
     {
         $pushToken = PushToken::findByReference($token);
-        $status = $ticket['status'] === 'error' ? 'error' : 'unknown';
-        if ($status === 'error' && $ticket['details']['error'] === 'DeviceNotRegistered') {
+        if ($ticket['status'] === 'error' && in_array($ticket['details']['error'], self::EXPIRE_FOR_STATUSES)) {
             try {
                 $pushToken->expire();
             } catch (\Exception $exception) {
-                Log::error("Unable to find token $token to expire");
+                Log::error("Unable to find token $token to expire.");
             }
         }
         $pushToken->touch();
         return \Relative\LaravelExpoPushNotifications\Models\PushNotification::create([
-            'batch_id' => $batchId,
+            'batch_id' => $batchId->toString(),
             'notification' => (array)$notification,
             'notifiable_id' => $notifiable->id,
             'notifiable_type' => get_class($notifiable),
             'token' => $token,
-            'status' => $status,
+            'status' => $ticket['status'],
             'ticket' => $ticket['id'] ?? '',
-            'error' => $status === 'error' ? $ticket['details']['error'] : null
+            'error' => $ticket['status'] === 'error' ? $ticket['details']['error'] : null
         ]);
-    }
-
-    /**
-     * @param array $tickets
-     * @param Collection $pushNotifications
-     * @param Collection $receipts
-     */
-    public function updatePushNotificationReceiptStatuses(array $tickets, Collection $pushNotifications, Collection $receipts): void
-    {
-        collect($tickets)->map(function ($ticket, $index) use ($pushNotifications, $receipts) {
-            $pushNotification = $pushNotifications[$index];
-            if (isset($ticket['id'])) {
-                $pushNotification->status = $receipts[$index]['status'] ?? 'unknown';
-                if ($pushNotification->status === 'error') {
-                    $pushNotification->errors = $receipts[$pushNotification->ticket]['details']['error'];
-                }
-                $pushNotification->save();
-            }
-        });
     }
 }
